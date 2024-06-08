118ffa29babd7504bf1098fca001f872f483a038
Anna Belousova <belusovanna@gmail.com>
2024-06-08 23:15:30 +0500

    added JUnit5 code example

--- src/test/java/github/SelenideGithubWikiTest.java
+++ src/test/java/github/SelenideGithubWikiTest.java
@@ -27,8 +27,19 @@ public class SelenideGithubWikiTest {
         $("#wiki-pages-filter").setValue("SoftAssertions");
 //        Откройте страницу SoftAssertions
         $(byText("SoftAssertions")).shouldBe(visible).click();
-//        проверьте что внутри есть пример кода для JUnit5
-        $("#wiki-body").shouldHave(text("JUnit5"));
+//        Проверьте что внутри есть пример кода для JUnit5
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:\n" +
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
     }
 }
